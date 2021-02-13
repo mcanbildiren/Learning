3e23db615893976710eb39a8736df1b57817aa56
mcanb <mcanb@DESKTOP-TSP3G1B>
2021-02-13 16:51:08 +0300

    qq

---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> ColorId = new Dictionary<string, int>();
+            ColorId.Add("Red",1);
+            ColorId.Add("Blue", 2);
+            foreach (var color in ColorId)
+            {
+                Console.WriteLine(color);
+            }
+        }
+    }
+}
